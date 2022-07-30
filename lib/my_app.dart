43be3af7ce59285c48e4735dcf58d2ce54f6d7a9
Georgii <matuageorge@gmail.com>
2022-07-30 22:37:06 +0200

    adding controller to webview

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:html/parser.dart' show parse;
+import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,29 +22,37 @@ class _MyAppState extends State<MyApp> {
   String? corsHeader = '';
   String pageContent = '';
   String operatingSystem = kIsWeb ? 'web' : Platform.operatingSystem;
+  String webViewUrl = '';
+  String title = 'App';
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
 
-  void _loadPage(String url) async {
-    try {
-      http.Response response = await http.get(
-        Uri.parse(url),
-      );
-      if (response.statusCode == 200) {
-        setState(() {
-          pageContent = response.body;
-          siteHeader =
-              parse(pageContent).getElementsByTagName('title')[0].text.trim();
+  Future<void> _loadUrl() async {
+    final WebViewController controller = await _controller.future;
+    controller.loadUrl(webViewUrl);
+    Future<String?> title = controller.getTitle();
+  }
+
+  Future<http.Response> _loadPage(String url) async {
+    if (url.isNotEmpty) {
+      try {
+        http.Response response = await http.get(
+          Uri.parse(url),
+        );
+        if (response.statusCode == 200) {
           corsHeader = response.headers['access-control-allow-origin'];
           corsHeader ??= 'None';
-        });
-      } else {
-        setState(() {
-          pageContent = 'Page could not be loaded';
-        });
+          return response;
+        } else {
+          return http.Response.bytes(
+              utf8.encode('Page could not be loaded'), 400);
+        }
+      } catch (e) {
+        return http.Response.bytes(
+            utf8.encode('Page could not be loaded'), 400);
       }
-    } catch (e) {
-      setState(() {
-        pageContent = 'Page could not be loaded';
-      });
+    } else {
+      return http.Response('Not loaded', 400);
     }
   }
 
@@ -52,103 +63,121 @@ class _MyAppState extends State<MyApp> {
       DeviceOrientation.portraitDown,
     ]);
     return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).requestFocus(FocusNode());
-      },
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('App'),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: SizedBox(
-                    child: Text(
-                      siteHeader,
-                      style: const TextStyle(
-                          fontSize: 21, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    'CORS Header: $corsHeader',
-                    style: const TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red),
-                  ),
-                ),
-                Expanded(
-                    flex: 5,
-                    child: SingleChildScrollView(
-                      child: Text(pageContent),
-                    )),
-                const Divider(color: Colors.grey),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 6,
-                      child: TextField(
-                        keyboardAppearance: Brightness.dark,
-                        controller: textEditingController,
-                        decoration: const InputDecoration(
-                          labelText: 'URL',
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          _loadPage(textEditingController.text);
-                        },
-                        child: const SizedBox(
-                          height: 25,
-                          width: 50,
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              'LOAD',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text('App'),
+              ),
+              body: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      FutureBuilder<http.Response>(
+                          future: _loadPage(webViewUrl),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<http.Response> snapshot) {
+                            print('webViewUrl: $webViewUrl');
+                            http.Response? response = snapshot.data;
+                            return (snapshot.hasData || snapshot.data != null)
+                                ? Expanded(
+                                    child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        response?.statusCode == 400
+                                            ? 'None'
+                                            : parse(response?.body)
+                                                .getElementsByTagName(
+                                                    'title')[0]
+                                                .text
+                                                .trim(),
+                                        style: TextStyle(
+                                            fontSize: 27,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        'CORS Header: ${response?.headers['access-control-allow-origin']}',
+                                        style: TextStyle(
+                                            fontSize: 21,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.red),
+                                      ),
+                                      Expanded(
+                                        child: Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              vertical: 8.0),
+                                          child: WebView(
+                                            onWebViewCreated:
+                                                _controller.complete,
+                                            onWebResourceError:
+                                                _controller.completeError,
+                                            gestureNavigationEnabled: true,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ))
+                                : Expanded(
+                                    child: Center(
+                                        child: CircularProgressIndicator()));
+                          }),
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextField(
+                              keyboardAppearance: Brightness.dark,
+                              controller: textEditingController,
+                              decoration: const InputDecoration(
+                                labelText: 'URL',
                               ),
                             ),
                           ),
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                webViewUrl = textEditingController.text;
+                                _loadUrl();
+                              });
+                            },
+                            child: const SizedBox(
+                              height: 25,
+                              width: 50,
+                              child: Align(
+                                alignment: Alignment.center,
+                                child: Text(
+                                  'LOAD',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 32.0),
+                        child: Text(
+                          'Application running on $operatingSystem',
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Application running on $operatingSystem',
-                    style: TextStyle(
-                      fontSize: 21.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    textEditingController.dispose();
-    super.dispose();
+              ),
+            )));
   }
 }
