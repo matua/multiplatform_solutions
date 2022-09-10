5e57ad1eb1d310f37d14fdbf628ce74bf3ba7ca5
Georgii <matuageorge@gmail.com>
2022-09-10 17:33:18 +0200

    narrow view bottomsheet

--- Apaptive_UI/lib/views/narrow_layout_card.dart
+++ Apaptive_UI/lib/views/narrow_layout_card.dart
@@ -16,54 +16,88 @@ class NarrowLayoutCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(imageUrl),
-              backgroundColor: Colors.transparent,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 21),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Wrap(
               children: [
-                Text(
-                  lastName,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 21),
+                ListTile(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  leading: const Icon(Icons.person),
+                  title: const Text('View Profile'),
                 ),
-                const SizedBox(
-                  width: 5,
+                ListTile(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  leading: const Icon(Icons.people),
+                  title: const Text('Friends'),
                 ),
-                Text(
-                  firstName,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 21,
-                      color: Colors.grey),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  email,
-                  style: const TextStyle(
-                    fontSize: 12,
-                    color: Colors.blueAccent,
-                  ),
+                ListTile(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  leading: const Icon(Icons.report),
+                  title: const Text('Report'),
                 ),
               ],
+            );
+          },
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(imageUrl),
+                backgroundColor: Colors.transparent,
+              ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 21),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    lastName,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 21),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    firstName,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 21,
+                        color: Colors.grey),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    email,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
